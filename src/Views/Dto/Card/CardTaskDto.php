1e4074a0617faa271d6604a1c693da2a88a3bd33
decole <decole@rambler.ru>
2026-09-10 01:38:00 +0300

    update docs

--- src/Views/Dto/Card/CardTaskDto.php
+++ src/Views/Dto/Card/CardTaskDto.php
@@ -6,4 +6,11 @@ namespace Planka\Bridge\Views\Dto\Card;
 
 use Planka\Bridge\Views\Dto\Task\TaskDto;
 
+/**
+ * Represents a single task item inside a task list on a card.
+ *
+ * This DTO extends TaskDto and is structurally identical to it because
+ * OpenAPI (swagger.json) defines a single 'Task' schema. It exists to
+ * maintain SDK-wide type consistency and Output Factory conventions for list items.
+ */
 class CardTaskDto extends TaskDto {}
